8a8084473812ea7ff7a948719741cfab58bf6f71
Solitai7e <p5ychomanti5@protonmail.com>
2021-06-24 23:34:17 +0000

    defer.hpp: ifdefs for exceptions support

--- defer.hpp
+++ defer.hpp
@@ -2,35 +2,40 @@
 #define DEFER_HPP
 
 #include <utility>
-#include <exception>
+
+#ifdef __cpp_exceptions
+    #include <exception>
+#endif
 
 
-#define DEFER_DEFCLASS(name, ...)                                         \
-    template <typename F>                                                 \
-    class name {                                                          \
-        const F func_;                                                    \
-                                                                          \
-        public:                                                           \
-            constexpr name(F &&f) noexcept : func_(std::forward<F>(f)) {} \
-            __VA_ARGS__                                                   \
-            name(name &&) = delete;                                       \
-            name &operator=(name &&) = delete;                            \
+#define DEFER_DEFCLASS(name, ...)                                        \
+    template <typename F>                                                \
+    class name {                                                         \
+        const F func;                                                    \
+                                                                         \
+        public:                                                          \
+            constexpr name(F &&f) noexcept : func(std::forward<F>(f)) {} \
+            __VA_ARGS__                                                  \
+            name(name &&) = delete;                                      \
+            name &operator=(name &&) = delete;                           \
     }
 
 namespace Defer {
     #if __cplusplus >= 202002L
-        DEFER_DEFCLASS(Exit, constexpr ~Exit() { func_(); });
+        DEFER_DEFCLASS(Exit, constexpr ~Exit() { this->func(); });
     #else
-        DEFER_DEFCLASS(Exit, ~Exit() { func_(); });
+        DEFER_DEFCLASS(Exit, ~Exit() { this->func(); });
     #endif
 
-    DEFER_DEFCLASS(Success, ~Success() {
-        if (std::uncaught_exceptions() == 0) func_();
-    });
+    #ifdef __cpp_exceptions
+        DEFER_DEFCLASS(Success, ~Success() {
+            if (std::uncaught_exceptions() == 0) this->func();
+        });
 
-    DEFER_DEFCLASS(Failure, ~Failure() {
-        if (std::uncaught_exceptions() > 0) func_();
-    });
+        DEFER_DEFCLASS(Failure, ~Failure() {
+            if (std::uncaught_exceptions() > 0) this->func();
+        });
+    #endif
 }
 
 
@@ -40,8 +45,12 @@ namespace Defer {
 
 #define DEFER_(policy) const Defer::policy DEFER_ANONNAME = [&]
 
-#define DEFER         DEFER_(Exit)
-#define DEFER_SUCCESS DEFER_(Success)
-#define DEFER_FAILURE DEFER_(Failure)
+
+#define DEFER DEFER_(Exit)
+
+#ifdef __cpp_exceptions
+    #define DEFER_SUCCESS DEFER_(Success)
+    #define DEFER_FAILURE DEFER_(Failure)
+#endif
 
 #endif
